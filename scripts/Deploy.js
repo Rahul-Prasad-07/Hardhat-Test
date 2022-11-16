a974dba40741780c2134a9b421ef15ce22189968
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2022-11-16 07:43:45 +0530

    details

--- scripts/Deploy.js
+++ scripts/Deploy.js
@@ -14,3 +14,10 @@ main().then(()=> process.exit(0)).catch((error)=>{
     console.error(error);
     process.exit(1);
 });
+
+/*
+ Value:               0 ETH
+  Gas used:            739749 of 739749
+  Block #1:            0xb45b0d6ccd8f016c08d54a9f7c28498d8b0754cc7e7c661515b2a56ba313bf1f
+
+ */
